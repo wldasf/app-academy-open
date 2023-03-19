6fe85f6c22f9862ec137521ed0255527e33ff462
wldasf <waleed.a.assaf@gmail.com>
2023-03-19 21:12:55 +0300

    18 solved

--- javascript-fundamentals/advanced-array-methods-practice/problems/18-two-d-product.js
+++ javascript-fundamentals/advanced-array-methods-practice/problems/18-two-d-product.js
@@ -26,7 +26,13 @@ console.log(twoDimensionalProduct(arr2)); // 88
 */
 
 let twoDimensionalProduct = function(arr) {
-    // Your code here
+  let product = 1;
+  arr.forEach(function (array) {
+    array.forEach(function (sArray) {
+      product *= sArray;
+    });
+  });
+  return product;
 };
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 

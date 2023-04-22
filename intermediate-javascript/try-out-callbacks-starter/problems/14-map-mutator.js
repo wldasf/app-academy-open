49533eeac3a0d2277c367c82c5adc3d0d988afec
wldasf <waleed.a.assaf@gmail.com>
2023-04-22 20:07:43 +0300

    14 done

--- intermediate-javascript/try-out-callbacks-starter/problems/14-map-mutator.js
+++ intermediate-javascript/try-out-callbacks-starter/problems/14-map-mutator.js
@@ -18,8 +18,10 @@ mapMutator(arr2, function (el, i) {
 console.log(arr2); // [ 0, 9, 20 ]
 *******************************************************************************/
 
-let mapMutator = function() {
-
+let mapMutator = function(arr, cb) {
+    arr.map(function (ele, i) {
+        arr[i] = cb(ele, i);
+    });
 };
 
 

2f9221812ca0416db4d1589f63e851715529a5ba
wldasf <waleed.a.assaf@gmail.com>
2023-06-11 22:50:38 +0300

    20 done

--- intermediate-javascript/try-out-callbacks-starter/problems/20-none.js
+++ intermediate-javascript/try-out-callbacks-starter/problems/20-none.js
@@ -27,15 +27,24 @@ let result4 = none([4, -5, 7, -1], function(n) {
 });
 console.log(result4);   // false
 *******************************************************************************/
-
+// loop through all arr elements and pass each element to callback
+// if all elements in an arr are false then return true
+    // have a counter that counts how many falses return relative to the length of the arr
 let none = function(arr, cb) {
-
-};
-
-
-
-
-
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let ele = arr[i];
+        if (cb(ele) == false) {
+            count++;
+        }
+    }
+    if (count == arr.length) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = none;

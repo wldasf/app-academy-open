8f34eacefd9823f81d1bfbfc49a69dada5d1f3b3
wldasf <waleed.a.assaf@gmail.com>
2023-06-13 19:57:53 +0300

    22 done

--- intermediate-javascript/try-out-callbacks-starter/problems/22-first-index.js
+++ intermediate-javascript/try-out-callbacks-starter/problems/22-first-index.js
@@ -22,8 +22,15 @@ let result3 = firstIndex(['canine', 'feline', 'tree'], function (s) {
 console.log(result3); // -1
 *******************************************************************************/
 
-let firstIndex = function() {
+let firstIndex = function(arr, cb) {
+    for (let i = 0; i < arr.length; i++) {
+        let elem = arr[i];
+        if (cb(elem)) {
+            return i;
+        }
+    }
 
+    return -1;
 };
 
 

ca03e38acf6f0186f670b62a94843fdc1c96f93a
wldasf <waleed.a.assaf@gmail.com>
2023-06-19 20:27:08 +0300

    call project done

--- intermediate-javascript/try-out-callbacks-starter/problems/24-my-simple-reduce.js
+++ intermediate-javascript/try-out-callbacks-starter/problems/24-my-simple-reduce.js
@@ -34,8 +34,13 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
   optional initial accumulator
 *******************************************************************************/
 
-let mySimpleReduce = function() {
-
+let mySimpleReduce = function(arr, cb) {
+    let accum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        let remaining = arr[i];
+        accum = cb(accum, remaining);
+    }
+    return accum;
 };
 
 

3530067c9987994d363ed41a9a70804c30ec750a
wldasf <waleed.a.assaf@gmail.com>
2023-06-23 18:44:31 +0300

    progress

--- intermediate-javascript/scope-problems-without-context-main/problems/05-arrow-mirror-array.js
+++ intermediate-javascript/scope-problems-without-context-main/problems/05-arrow-mirror-array.js
@@ -10,6 +10,11 @@ mirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a
 
 const mirrorArray = (array) => {
   // Your code here
+  const copy = array;
+  for (let i = copy.length - 1; i >= 0; i--) {
+    array.push(copy[i]);
+  }
+  return copy;
 };
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
--- intermediate-javascript/scope-problems-without-context-main/problems/06-closure-sandwiches.js
+++ intermediate-javascript/scope-problems-without-context-main/problems/06-closure-sandwiches.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Let's practice writing closures by creating a function named `sandwichMaker()`.
-This function will return another function that will accept a string to add 
+This function will return another function that will accept a string to add
 to the sandwich order (which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 
@@ -19,6 +19,10 @@ sandwich2("pb") // => "One sandwich with tomato and pb"
 
 function sandwichMaker() {
   // Your code here
+  function sandwich(word) {
+    console.log(word);
+  }
+  return sandwich();
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/

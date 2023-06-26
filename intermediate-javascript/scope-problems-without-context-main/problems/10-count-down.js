1425d6f067d1e61fb0df2940d0bca84ecd61afac
wldasf <waleed.a.assaf@gmail.com>
2023-06-26 12:25:01 +0300

    10 done

--- intermediate-javascript/scope-problems-without-context-main/problems/10-count-down.js
+++ intermediate-javascript/scope-problems-without-context-main/problems/10-count-down.js
@@ -1,11 +1,11 @@
 /***********************************************************************
 Write a function named: countDownTimer(n). This function will represent a count
-down of days till the New Year. The countDownTimer function will 
+down of days till the New Year. The countDownTimer function will
 take in a number argument (n) the first time it is called and if that
 number is greater than 0 the countDownTimer will return a function.
 
-The function returned by countDownTimer can then be invoked n times before it 
-returns a string of "Happy New Year!". Look closely at how this function is 
+The function returned by countDownTimer can then be invoked n times before it
+returns a string of "Happy New Year!". Look closely at how this function is
 invoked below:
 
 Example 1:
@@ -27,9 +27,28 @@ Example 4:
   console.log(threeDays()); // prints "Happy New Year!"
 
 ***********************************************************************/
-function countDownTimer(num) {
-  // Your code here
+function countDownTimer(n) {
+  if (n <= 0) {
+    return "Happy New Year!";
+  }
+
+  let count = n;
+
+  return function() {
+    if (count <= 0) {
+      return "Happy New Year!";
+    }
+
+    count--;
+    return countDownTimer(count);
+  };
 }
+
+
+
+
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {

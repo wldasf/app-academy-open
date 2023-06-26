2664d366cc6042a71da4dd9df557777a96972108
wldasf <waleed.a.assaf@gmail.com>
2023-06-26 10:41:40 +0300

    09 done

--- intermediate-javascript/scope-problems-without-context-main/problems/09-interrupter.js
+++ intermediate-javascript/scope-problems-without-context-main/problems/09-interrupter.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function named interrupter that accepts a single parameter `interruptingWord`: 
+Write a function named interrupter that accepts a single parameter `interruptingWord`:
   `interrupter(interruptingWord)`
-The interrupter function should return a function. 
+The interrupter function should return a function.
 When the function returned by interrupter is called with a sentence,
 the sentence will be returned with the original interruptingWord inserted
 between each word in the sentence.
@@ -12,7 +12,7 @@ console.log(rudePerson("how are you")); // prints "how what are what you"
 console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 
-Invoking the interrupter function again: 
+Invoking the interrupter function again:
 let rudePerson2 = interrupter("yo"); // => returns a function
 console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
@@ -21,6 +21,9 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 function interrupter(interruptingWord) {
   // Your code here
+  return function (sentence) {
+    return sentence.split(" ").join(" " + interruptingWord + " ");
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/

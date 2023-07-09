c2206e885e45c4a36c3ecc23544a79c31c1c75bc
wldasf <waleed.a.assaf@gmail.com>
2023-07-10 01:18:43 +0300

    scope and closures project DONE

--- intermediate-javascript/scope-problems-without-context-main/problems/20-curried-sum.js
+++ intermediate-javascript/scope-problems-without-context-main/problems/20-curried-sum.js
@@ -43,8 +43,21 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
 ***********************************************************************/
 
 function curriedSum(numArgs) {
-  // Your code here
-
+  let numbers = [];
+  function _curriedSum(num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      let sum = 0;
+      numbers.forEach(function (elem) {
+        sum += elem;
+      });
+      return sum;
+    }
+    else {
+      return _curriedSum;
+    }
+  }
+  return _curriedSum;
 }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {

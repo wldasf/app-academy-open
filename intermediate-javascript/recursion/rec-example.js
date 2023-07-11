eb56ef9c082b5bf4b1168f8b9ba5a869a94ab1d2
wldasf <waleed.a.assaf@gmail.com>
2023-07-11 13:36:52 +0300

    recursion notes

--- intermediate-javascript/recursion/rec-example.js
+++ intermediate-javascript/recursion/rec-example.js
@@ -4,16 +4,57 @@ function foo() {
   console.log("e");
 }
 
-
 function bar() {
   console.log("b");
   baz();
   console.log("d");
 }
 
-
 function baz() {
   console.log("c");
 }
 
-foo();
+
+// default parameter
+function growNumber(num, amount = false) {
+  if (amount) {
+    console.log(num);
+  }
+}
+
+
+// stack overflow
+function pythagoreanCup() {
+  pythagoreanCup();
+}
+
+
+// indirect recursion
+function didYouDoTheThing() {
+  ofCourseIDidTheThing();
+}
+function ofCourseIDidTheThing() {
+  didYouDoTheThing();
+}
+
+
+// iterative vs recursive approach
+// // iterative example
+// countdown(startingNumber) {
+//   for (let i = startingNumber; i > 0; i--) {
+//     console.log(i);
+//   }
+
+//   console.log("Time's up!");
+// }
+
+// // recursive approach
+// countdown(startingNumber) {
+//   if (startingNumber === 0) {
+//     console.log("Time's up!");
+//     return;
+//   }
+
+//   console.log(startingNumber);
+//   countdown(startingNumber - 1);
+// }

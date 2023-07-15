e72be2f816eb0c90bf8ae7499f92fcb8a17b8ef5
wldasf <waleed.a.assaf@gmail.com>
2023-07-15 16:39:13 +0300

    02 recursion project done

--- intermediate-javascript/recursion/starter/problems/02-sum-array.js
+++ intermediate-javascript/recursion/starter/problems/02-sum-array.js
@@ -11,7 +11,13 @@ sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
 
 // your code here
-  
+function sumArray(arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
+  return arr[0] + sumArray(arr.slice(1));
+}
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;

ed751dd4429340785b89cab1041b8a800440bdbd
wldasf <waleed.a.assaf@gmail.com>
2023-08-02 23:49:33 +0300

    11 done

--- intermediate-javascript/recursion/starter/problems/11-flatten.js
+++ intermediate-javascript/recursion/starter/problems/11-flatten.js
@@ -12,10 +12,20 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 
 // your code here
 function flatten(array) {
-if (array.length === 0) {
-  return [];
-}
-return flatten()
+  if (array.length === 0) {
+    return [];
+  }
+
+  const first_element = array[0];
+  if (Array.isArray(first_element)) {
+    const flattened_first_element = flatten(first_element);
+    const other_flattened_elements = flatten(array.slice(1));
+    return flattened_first_element.concat(other_flattened_elements);
+  }
+  else {
+    const rest = flatten(array.slice(1));
+    return [first_element].concat(rest);
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/

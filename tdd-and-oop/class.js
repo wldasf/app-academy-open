526154abf604817e9cd2f5c21079efbcb61ee096
wldasf <assafw25@gmail.com>
2023-08-27 20:26:02 +0300

    notes

---
+++ tdd-and-oop/class.js
@@ -0,0 +1,12 @@
+class Book {
+    constructor(title, series, author) {
+        this.title = title;
+        this.series = series;
+        this.author = author;
+    }
+}
+
+const fellowshipOfTheRing = new Book(
+    'The Fellowship of the Ring',
+    'The Lord of the Rings',
+    'J.R.R. Tolkien');

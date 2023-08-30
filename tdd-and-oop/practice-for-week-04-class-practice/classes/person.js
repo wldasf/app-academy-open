0275ebdd3306476d675d39229757362d6464f068
wldasf <assafw25@gmail.com>
2023-08-30 19:58:22 +0300

    update

--- tdd-and-oop/practice-for-week-04-class-practice/classes/person.js
+++ tdd-and-oop/practice-for-week-04-class-practice/classes/person.js
@@ -10,15 +10,19 @@ class Person {
     console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
   }
 
-  static introducePeople(...persons) {
+  static introducePeople(persons) {
     if (!Array.isArray(persons)) {
-      console.log("introducePeople only takes an array as an argument.");
+      throw new Error("introducePeople only takes an array as an argument.");
     }
-    for (let i = 0; i < persons.length; i++) {
-      Person.introduce(persons[i]);
+    for (let person of persons) {
+      if (!(person instanceof Person)) {
+        throw new Error("All items in array must be Person class instances.");
+      }
+      person.introduce();
     }
   }
 }
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/

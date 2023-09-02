09df89920e37d586fdc2e950c486163b71e5a2b5
wldasf <assafw25@gmail.com>
2023-09-02 06:50:58 +0300

    inheritance practice done

--- tdd-and-oop/practice-for-week-04-inheritance/classes/student.js
+++ tdd-and-oop/practice-for-week-04-inheritance/classes/student.js
@@ -1,6 +1,25 @@
 const Person = require('./person');
 
 // Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName, major, GPA);
+    this.major = major;
+    this.GPA = GPA;
+  }
+  static compareGPA(student1, student2) {
+    if (student1.GPA === student2.GPA) {
+      return "Both students have the same GPA";
+    }
+    if (student1.GPA > student2.GPA) {
+      return `${student1.firstName} ${student1.lastName} has the higher GPA.`;
+    }
+    if (student1.GPA < student2.GPA) {
+      return `${student2.firstName} ${student2.lastName} has the higher GPA.`;
+    }
+  }
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- tdd-and-oop/practice-for-week-04-inheritance/classes/teacher.js
+++ tdd-and-oop/practice-for-week-04-inheritance/classes/teacher.js
@@ -1,6 +1,21 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName, subject, yearsOfExperience);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let sum = 0;
+    for (let i = 0; i < teachers.length; i++) {
+      sum += teachers[i].yearsOfExperience;
+    }
+    return sum;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/

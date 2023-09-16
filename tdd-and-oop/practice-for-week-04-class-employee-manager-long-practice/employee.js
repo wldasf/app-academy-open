3bfa0eb6f926aab641306752239797b7bc15617c
wldasf <waleed.a.assaf@gmail.com>
2023-09-16 14:25:30 +0300

    update

--- tdd-and-oop/practice-for-week-04-class-employee-manager-long-practice/employee.js
+++ tdd-and-oop/practice-for-week-04-class-employee-manager-long-practice/employee.js
@@ -4,6 +4,11 @@ class Employee {
         this.salary = salary;
         this.title = title;
         this.manager = manager;
+
+
+        if (this.manager) {
+            this.manager.addEmployee(this);
+        }
     }
 }
 

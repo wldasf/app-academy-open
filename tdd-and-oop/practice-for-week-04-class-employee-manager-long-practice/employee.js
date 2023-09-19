3063ccadee98b5bded6c4b2be99cf28515ebb13d
ps.waleed.assaf <waleed.assaf@progressoft.com>
2023-09-19 12:35:53 +0300

    manager employee 4a practice done

--- tdd-and-oop/practice-for-week-04-class-employee-manager-long-practice/employee.js
+++ tdd-and-oop/practice-for-week-04-class-employee-manager-long-practice/employee.js
@@ -10,6 +10,16 @@ class Employee {
             this.manager.addEmployee(this);
         }
     }
+
+    calculateBonus(multiplier) {
+        return (this.salary) * multiplier;
+    }
 }
 
 module.exports = Employee;
+
+const raph = new Employee('Raphael', 90000, 'Ninja');
+const donny = new Employee('Donatello', 85000, 'Grasshopper');
+
+console.log(raph.calculateBonus(0.25)); // => 22500
+console.log(donny.calculateBonus(0.15)); // => 12750

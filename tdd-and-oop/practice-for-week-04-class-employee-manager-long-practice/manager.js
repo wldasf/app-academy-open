a951878d111d404bade96c1ed79684c250d1d042
user <user@pop-os.localdomain>
2023-09-20 22:04:24 +0300

    update

--- tdd-and-oop/practice-for-week-04-class-employee-manager-long-practice/manager.js
+++ tdd-and-oop/practice-for-week-04-class-employee-manager-long-practice/manager.js
@@ -1,9 +1,27 @@
 const Employee = require("./employee");
 
 class Manager extends Employee{
-    constructor(name, salary, title, employees = []) {
-        super(name, salary, title);
+    constructor(name, salary, title, manager = null, employees = []) {
+        super(name, salary, title, manager);
         this.employees = employees;
+    }
+
+    _totalSubSalary() {
+        let sum = 0;
+        for (const employee of this.employees) {
+            if (employee instanceof Manager) {
+                sum += this.salary + this._totalSubSalary;
+            }
+            else {
+                sum += this.salary;
+            }
+        }
+        return sum;
+    }
+
+    calculateBonus(multiplier) {
+        let total = this.salary + this._totalSubSalary();
+        return total * multiplier;
     }
 
     addEmployee(employee) {

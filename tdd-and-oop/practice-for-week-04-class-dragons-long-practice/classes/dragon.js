322b75a4bc06b26a0f241a6b59a9f60f6a52c90c
wldasf <waleed.a.assaf@gmail.com>
2023-09-21 09:52:14 +0300

    friendly and evil dragon practice phase 1 done

--- tdd-and-oop/practice-for-week-04-class-dragons-long-practice/classes/dragon.js
+++ tdd-and-oop/practice-for-week-04-class-dragons-long-practice/classes/dragon.js
@@ -1,4 +1,21 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...arg) {
+    let names = [];
+    for (let i = 0; i < arg.length; i++) {
+      names.push(arg[i].name);
+    }
+    return names;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
